68526b06910b13b34d98a04aa95cef019b10c91a
manipah <maxwell.anipah@idnow.de>
2018-01-16 16:01:12 +0100

    how to get values from route+s

--- routing-start/src/app/users/user/user.component.ts
+++ routing-start/src/app/users/user/user.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-import { Subscription } from 'rxjs/SUbscription';
+import { Subscription } from 'rxjs/Subscription';
+
+/*
+  So basically to retrieve url like /user/:id/:name
+  we can either do it with the approach of getting params on ngOnInit without Subscription, if the Component does not call itself
+  if so we have to use a Subscription (observable) and destory it afterwards
+ */
 
 @Component({
   selector: 'app-user',
@@ -17,14 +23,14 @@ export class UserComponent implements OnInit, OnDestroy {
     this.user = {
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name']
-    }
+    };
 
     this.paramSubscription  = this.route.params.subscribe(
         (params: Params) => {
           this.user.id = params['id'];
           this.user.name = params['name'];
         }
-    )
+    );
   }
 
   ngOnDestroy() {
